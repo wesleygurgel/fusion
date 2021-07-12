ab9360d83c370bee3707bf97f31da574e81435d1
Wesley Gurgel <wesleygurgel27@gmail.com>
2021-07-12 11:22:44 -0300

    (FEAT): Criação do Models + Visualização no /admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Cargo, Funcionario, Servico
+
 
 # Register your models here.
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ['cargo', 'ativo', 'modificado']
+
+
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ['servico', 'icone', 'modificado', 'ativo']
+
+
+@admin.register(Funcionario)
+class FuncionarioAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'cargo', 'modificado', 'ativo']
