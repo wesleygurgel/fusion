a98bfc38f1d43cd8ab56ea1f33f8af500c2274bf
Wesley Gurgel <wesleygurgel27@gmail.com>
2021-07-12 13:07:53 -0300

    (FEAT): Context Manager + Desafio Features (Colocando os conteúdos)

--- core/views.py
+++ core/views.py
@@ -1,7 +1,14 @@
 from django.views.generic import TemplateView
+from .models import Servico, Funcionario, Features
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        context['servicos'] = Servico.objects.order_by('?').all()
+        context['funcionario'] = Funcionario.objects.order_by('?').all()
+        context['features'] = Features.objects.order_by('?').all()
+        return context
 
